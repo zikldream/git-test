9fd7117dd1443599d825702f1e9d794b4f1ee93c
ZikL <1622873925@qq.com>
2022-05-16 14:08:23 +0800

    first commit

---
+++ src/main/java/com/zzp/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zzp.git;
+
+/**
+ * @author 朱子鹏
+ * @create 2022-05-16 13:24
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
