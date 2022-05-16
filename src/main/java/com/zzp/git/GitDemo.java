93cba336ec70050a24dab852dac08e866939f1f6
ZikL <1622873925@qq.com>
2022-05-16 14:21:44 +0800

    hot-fix first commit

--- src/main/java/com/zzp/git/GitDemo.java
+++ src/main/java/com/zzp/git/GitDemo.java
@@ -8,5 +8,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
         System.out.println("hello git2!!!");
+        System.out.println("hello git4!!!");
     }
 }
